1f6cc6eaeb083a1c2f0fbe71ca4195ff33285be6
anibalarevalo <79030561+anibalarevalo@users.noreply.github.com>
2024-05-08 17:27:52 -0400

    finish service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import { ConnectableObservable } from 'rxjs';
-
+import { PokemonService } from './services/pokemon.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,32 +8,37 @@ import { ConnectableObservable } from 'rxjs';
 export class AppComponent implements OnInit {
   pokemons: any = [];
   pokemonsFiltrados: any = [];
-  n:number = 1
-  constructor(private http: HttpClient) { }
+  n: number = 1;
   mostrarMas = true;
 
- 
+  constructor(private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
-    this.http.get('https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0')
-    .subscribe( (res:any) => {this.pokemonsFiltrados=[... res.results].splice(0,25) ; this.pokemons = res.results
-    }, err => console.log(err))
+    this.pokemonService.getData().subscribe({
+      next: (data: any) => {
+        this.pokemonsFiltrados = [...data.results].splice(0, 25);
+        this.pokemons = data.results;
+      },
+      error: (error: any) => {
+        console.log(error);
+      }
+    })
   }
 
-  searchPokemon(e:string){
-    if (e){
-      this.pokemonsFiltrados = this.pokemons.filter( (p:any) => p.name.toLowerCase().includes( e.toLowerCase()) ).splice(0,25);
+  searchPokemon(e: string) {
+    if (e) {
+      this.pokemonsFiltrados = this.pokemons.filter((p: any) => p.name.toLowerCase().includes(e.toLowerCase())).splice(0, 25);
       this.mostrarMas = false
-    }else{
-      this.pokemonsFiltrados = [... this.pokemons].splice(0,25)
+    } else {
+      this.pokemonsFiltrados = [... this.pokemons].splice(0, 25)
       this.mostrarMas = true
-    } 
-    console.log (this.mostrarMas)
+    }
+    console.log(this.mostrarMas)
 
   }
 
-  loadMore(){
-    this.pokemonsFiltrados = this.pokemonsFiltrados.concat([... this.pokemons].splice(25*this.n, 25))
-    this.n+=1
+  loadMore() {
+    this.pokemonsFiltrados = this.pokemonsFiltrados.concat([... this.pokemons].splice(25 * this.n, 25))
+    this.n += 1
   }
 }
